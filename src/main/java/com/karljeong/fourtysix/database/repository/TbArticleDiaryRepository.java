f3c43bbf67863ed21ae15b2d8c17b593740609cd
hwha <hwha@bitnine.net>
2020-03-06 00:32:55 +0900

    수정 검색조건 적용을 위한 상속 추가
    JpaSpecificationExecutor 를 상속받아 검색조건을 처리할수 있도록 수정중

--- src/main/java/com/karljeong/fourtysix/database/repository/TbArticleDiaryRepository.java
+++ src/main/java/com/karljeong/fourtysix/database/repository/TbArticleDiaryRepository.java
@@ -2,6 +2,7 @@ package com.karljeong.fourtysix.database.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,7 +10,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.karljeong.fourtysix.database.entity.TbArticleDiary;
 
 @RepositoryRestResource(collectionResourceRel = "articleEntity", path = "tbArticleDiary")
-public interface TbArticleDiaryRepository extends PagingAndSortingRepository<TbArticleDiary, Long> {
+public interface TbArticleDiaryRepository extends PagingAndSortingRepository<TbArticleDiary, Long>, JpaSpecificationExecutor<TbArticleDiary> {
 
     List<TbArticleDiary> findByArticleTitle(@Param("articleTitle") String articleTitle);
 
